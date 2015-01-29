e97fe470274a78a6fa0ecac59c462024d6eb752c
sbenavidesvazquez <sbenavidesvazquez@eniac20.danielcastelao.org>
2015-01-29 08:47:15 +0100

    commit 3

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -9,17 +9,17 @@ public class ExamenCOD {
      */
     public static void main(String[] args) {
  int iN = 11;
-            	if (metodoMio(iN))
+            	if (elMetodo(iN))
 System.out.println("Es numero primo");
 else
  System.out.println("NO es numero primo");           	 
           	 
 for (int x=2;x<1000;x++) {
-if (metodoMio(x))
+if (elMetodo(x))
 System.out.print(x + " ");                         	 
 }         	 
 	}
-public static boolean metodoMio(int variable){
+public static boolean elMetodo(int variable){
 int j = 2;
 boolean aux=true;
 while ((aux) && (j!=variable)){
